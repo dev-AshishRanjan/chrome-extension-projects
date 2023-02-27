e2fd1089bee4c8b7be988c87d75504fdb44eb556
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-02-28 00:37:54 +0530

    updating Bugify

--- Bugify/bugify.js
+++ Bugify/bugify.js
@@ -1,24 +1,42 @@
+// M - 1
+// function start(){
+//     chrome.tabs.onUpdated.addListener(({tabId, changeInfo, tab}) => {
+//         console.log(changeInfo);
+//         chrome.tabs.detectLanguage(
+//             tabId,(language)=>{
+//                 console.log(language);
+//             }
+//           )
+//         chrome.tabs.insertCSS(tabId,{
+//             file:"bugify.css"
+//         },()=>{console.log("Bugify Active")})
+//     });
+// }
+// function end(){
+//     chrome.tabs.onUpdated.addListener(({tabId, changeInfo, tab}) => {
+//         console.log(changeInfo);
+//         chrome.tabs.insertCSS(tabId,{
+//             file:"nonBugify.css"
+//         },()=>{console.log("Bugify Deactive")})
+//     });
+// }
+
+// M - 2
 function start(){
-    chrome.tabs.onUpdated.addListener(({tabId, changeInfo, tab}) => {
-        console.log(changeInfo);
-        chrome.tabs.detectLanguage(
-            tabId,(language)=>{
-                console.log(language);
-            }
-          )
-        chrome.tabs.insertCSS(tabId,{
+    chrome.tabs.executeScript(null, {
+        code: chrome.tabs.insertCSS(null,{
             file:"bugify.css"
         },()=>{console.log("Bugify Active")})
     });
 }
 function end(){
-    chrome.tabs.onUpdated.addListener(({tabId, changeInfo, tab}) => {
-        console.log(changeInfo);
-        chrome.tabs.insertCSS(tabId,{
-            file:"nonBugify.css"
-        },()=>{console.log("Bugify Deactive")})
+    chrome.tabs.executeScript(null, {
+        code: chrome.tabs.removeCSS(null,{
+            file:"bugify.css"
+        },()=>{console.log("Bugify Active")})
     });
 }
+
 var count=0;
 chrome.browserAction.onClicked.addListener(function(tab) {
     count++;
@@ -26,7 +44,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         start();
     }else{
         end();
-        chrome.runtime.reload();
+        // chrome.runtime.reload();
     }
 });
 // chrome.tabs.onSelectionChanged.addListener(({tabId,selectInfo})=>{
